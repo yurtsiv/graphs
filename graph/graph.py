b2567519789f48a121328012ea54b7e11428b42b
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-05-31 18:08:54 +0200

    remove_node method

--- graph/graph.py
+++ graph/graph.py
@@ -4,18 +4,32 @@ class Graph:
     self.weighted = weighted
     self.directed = directed
   
+  def raise_no_node_exception(self, key):
+    raise Exception("There is no node " + str(key) + " in the graph")
+  
   def add_node(self, key):
     if key in self.nodes:
-      raise Exception("Node with key " + str(key) + " already exists")
+      self.raise_no_node_exception(key)
 
     self.nodes[key] = []
 
-
+  def remove_node(self, key):
+    if not key in self.nodes:
+      self.raise_no_node_exception(key) 
+    
+    for edge in self.nodes[key]:
+      liked_node = edge[0]
+      self.nodes[liked_node] = list(
+        edge for edge in self.nodes[liked_node] if edge[0] != key
+      )
+    
+    del self.nodes[key]
+        
   def add_edge(self, key1, key2, weight=0):
     if not key1 in self.nodes:
-      raise Exception("There is no node " + str(key1) + " in the graph")
+      self.raise_no_node_exception(key1)
     if not key2 in self.nodes:
-      raise Exception("There is no node " + str(key2) + " in the graph")
+      self.raise_no_node_exception(key2)
 
     if self.directed:
       self.nodes[key1].append((key2, weight))
--- test/test_graph.py
+++ test/test_graph.py
@@ -36,3 +36,20 @@ class TestGraphMethods(unittest.TestCase):
       1: [(2, 0)],
       2: []
     })
+
+  def test_remove_node(self):
+    graph = Graph()
+    graph.add_node(1)
+    graph.add_node(2)
+    graph.add_node(3)
+
+    graph.add_edge(1,2)
+    graph.add_edge(2,3)
+    graph.add_edge(1,3)
+
+    graph.remove_node(1)
+
+    self.assertEqual(graph.nodes, {
+      2: [(3,0)],
+      3: [(2, 0)]
+    })
